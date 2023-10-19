75b9e8948a3d7c3572e6828768bd2a3a149627ce
liuyishengalan <liuyishengalan@hotmail.com>
2023-10-19 12:43:08 -0700

    fix bug in tracer extractor

--- traces_extractor/page2delta.py
+++ traces_extractor/page2delta.py
@@ -6,7 +6,7 @@ with open("page.txt", "r") as f_in, open("delta.txt", "w") as f_out:
         pc = int(elements[0], 16)
         page_addr = int(elements[1], 16)
         if prev_page_addr is not None:
-            delta = page_addr - prev_page_addr
+            delta = (page_addr - prev_page_addr)//4096
             f_out.write(f"{hex(pc)} {delta}\n")
         else:
             f_out.write(f"{hex(pc)} {0}\n")
